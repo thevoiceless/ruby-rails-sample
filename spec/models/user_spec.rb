1b49a9fdf3c87929537ab9979cdfc58a38d9a1ae
Riley Moses <victi.resurgo@gmail.com>
2012-07-26 12:12:42 -0600

    Test email address downcasting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ require 'spec_helper'
 describe User do
 	before { @user = User.new(name: "Example User", email: "user@example.com",
 		password: "foobar", password_confirmation: "foobar") }
+	
 	subject { @user }
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
